c5b4e7320928c30763981b885f63212c94a5ae09
wain-aina <57414251+wain-aina@users.noreply.github.com>
2022-12-03 12:34:09 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+mongoose.connect("mongodb://0.0.0.0:27017/BlogDB", {useNewUrlParser: true});
+
+const articleSchema = ({
+    title: String,
+    content: String
+});
+
+const Article = new mongoose.model("Article", articleSchema);
+
+// Request targetting all articles
+
+app.route("/articles")
+.get(function(req, res) {
+    Article.find(function(err,foundArticles){
+        if(!err){
+            res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+    });
+})
+.post(function(req,res){
+    const article1 = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+    article1.save(function(err){
+        if (!err){
+            res.send("Successfully added new article")
+        }
+    });
+})
+.delete(function(req, res){
+    Articles.deleteMany(function(err){
+        if (!err){
+            res.send("Delete Successful");
+        } else {
+            res.send("Error detected")
+        }
+    });
+});
+
+// Targetting a specific article
+app.route("/articles/:articleTitle")
+    .get(function(req, res){
+        Article.findOne({title:req.params.articleTitle}, function(err, foundArticle){
+            if (foundArticle){
+                res.send(foundArticle);
+            } else {
+                res.send("not found");
+            }
+        });
+    })
+    .put(function(req, res){
+    Article.updateOne({title:req.params.articleTitle},
+        {title:req.body.title, content:req.body.content},
+        {overwrite:true}, function(err){
+        if(!err){
+            res.send("Successfully updated");
+        } else {}
+        res.send("Ran into error.Try Again");
+        });
+    })
+    .patch(function(req, res){
+        Article.updateOne({title:req.params.articleTitle},
+            {$set:req.body}, function(err){
+            if(!err){
+                res.send("Successfully updated");
+            } else {
+                res.send("Error occurred");
+            }
+            });
+    })
+    .delete(function(req, res){
+        Article.deleteOne({title:req.params.articleTitle}, function(err){
+            if(!err){
+                res.send("Article deleted");
+            } else {}
+            res.send("Try Again");
+        });
+    });
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
